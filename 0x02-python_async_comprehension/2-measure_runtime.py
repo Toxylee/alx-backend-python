63ff1e2fdb90674e76f5cbaf073e2301050ae227
Toxylee <toxylee20021@gmail.com>
2023-10-10 21:02:57 +0000

    Async Comprehension

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''Module - Task 2
+'''
+import asyncio
+import time
+from importlib import import_module as impmod
+
+async_comprehension = impmod('1-async_comprehension').async_comprehension
+
+async def measure_runtime() -> float:
+    '''Executes async_comprehension 4 times and measures the
+    total execution time.
+    '''
+    start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    return time.time() - start_time

fa3e1608ccce3b7ca1eaf66ff91c66804c68f028
Toxylee <toxylee20021@gmail.com>
2023-10-28 18:59:26 +0000

    Variable annotation

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Module 0 Task
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''Adds two floating-point numbers.
+    '''
+    return a + b
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Module 2 task
+'''
+
+
+def floor(a: float) -> int:
+    '''Computes the floor of a floating-point number.
+    '''
+    return int(a)
